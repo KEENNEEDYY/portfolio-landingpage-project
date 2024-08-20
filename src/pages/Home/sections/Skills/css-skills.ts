34a715f1809b12d80e1a03c02efe81f1b71ee42f
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2024-08-20 11:45:47 -0300

    styling css organize
    Signed-off-by: KEENNEEDYY <kennedy.costa.valenet@gmail.com>

---
+++ src/pages/Home/sections/Skills/css-skills.ts
@@ -0,0 +1,5 @@
+export const section = "pt-[4rem] md:pt-[8rem] pb-[5rem] bg-[#09101a]";
+
+export const skill_itens_container = "w-[80%] mx-auto pt-[4rem] md:pt-[8rem] grid grid-cols-1 md:grid-cols-2 gap-[2rem] items-center";
+
+export const skill_card = "h-[100%] flex flex-col justify-between";
